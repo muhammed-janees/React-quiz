3673a170582f023ad7114332a28414c96a42f7dc
muhammed-janees <muhammedjanees313@gmail.com>
2023-11-06 21:11:17 +0530

    quiz-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import quizData from './quizData'
+
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+
+  const handleAnswerButtonClick = (selectedAnswer) => {
+    if (selectedAnswer === quizData[currentQuestion].correctAnswer) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < quizData.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const playAgain = () => {
+    setShowScore(false);
+    setCurrentQuestion(0);
+    setScore(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="quiz-container">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {quizData.length}
+            <button onClick={playAgain}>Try Again</button>
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>{currentQuestion + 1}</span>/{quizData.length}
+              </div>
+              <div className="question-text">{quizData[currentQuestion].question}</div>
+            </div>
+            <div className="answer-section">
+              <div className="button-row">
+                <div className="button-column">
+                  {quizData[currentQuestion].options.slice(0, 2).map((option, index) => (
+                    <button key={index} onClick={() => handleAnswerButtonClick(option)}>
+                      {option}
+                    </button>
+                  ))}
+                </div>
+                <div className="button-column">
+                  {quizData[currentQuestion].options.slice(2, 4).map((option, index) => (
+                    <button key={index + 2} onClick={() => handleAnswerButtonClick(option)}>
+                      {option}
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/quizData.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: 'What is React JS?',
+        options: [
+            'A JavaScript library for building user interfaces',
+            'A backend framework for Node.js',
+            'A programming language for web development',
+            'A database management system',
+        ],
+        correctAnswer: 'A JavaScript library for building user interfaces',
+    },
+    {
+        question: 'What is JSX in React?',
+        options: [
+            'JavaScript XML',
+            'A programming language',
+            'A database query language',
+            'A styling language for React components',
+        ],
+        correctAnswer: 'JavaScript XML',
+    },
+    {
+        question: 'What of the following is used in React to increase performance?',
+        options: [
+            'Original DOM',
+            'Virtual DOM',
+            'Both A and B',
+            'None of the above',
+        ],
+        correctAnswer: 'Virtual DOM',
+    },
+    {
+        question: 'Which of the following acts as the input of a class-based component?',
+        options: [
+            'Class',
+            'Factory',
+            'Render',
+            'Props',
+        ],
+        correctAnswer: 'Props',
+    },
+    {
+        question: 'Which of the following keyword is used to create a class inheritance?',
+        options: [
+            'Create',
+            'Inherits',
+            'Extends',
+            'This',
+        ],
+        correctAnswer: 'Extends',
+    },
+    {
+        question: 'What is a state in React?',
+        options: [
+            'A permanent storage',
+            'Internal storage of the component',
+            'External storage of the component',
+            'None of the above',
+        ],
+        correctAnswer: 'Internal storage of the component',
+    },
+    {
+        question: 'What are the two ways to handle data in React?',
+        options: [
+            'State & Props',
+            'Services & Components',
+            'State & Services',
+            'State & Component',
+        ],
+        correctAnswer: 'State & Props',
+    },
+    {
+        question: 'Which of the following option is correct in the case of the Babel?',
+        options: [
+            'Babel is a Compiler',
+            'Babel is a Transpilar',
+            'None of the above',
+            'Both A and B are correct',
+        ],
+        correctAnswer: 'Both A and B are correct',
+    },
+    {
+        question: 'Does React create a VIRTUAL DOM in the memory?',
+        options: [
+            'True',
+            'False',
+            'Can be true or false',
+            'Cannot say',
+        ],
+        correctAnswer: 'True',
+    },
+    {
+        question: 'What is true for the keys given to a list of elements in React?',
+        options: [
+            'Unique in the DOM',
+            'Unique among the siblings only',
+            'Do not require to be unique',
+            'None of the above',
+        ],
+        correctAnswer: 'Unique among the siblings only',
+    },
+];
+
+
+export default quizData;
